7a881311ed871f07ebdcdb62f6c1a36a76cab795
LucasDev13 <48536745+LucasDev13@users.noreply.github.com>
2019-03-13 22:03:08 -0300

    Update BalancoTrimestral.java

--- aula-poo/src/br/com/aulapoo/ex1/BalancoTrimestral.java
+++ aula-poo/src/br/com/aulapoo/ex1/BalancoTrimestral.java
@@ -13,16 +13,17 @@ public class BalancoTrimestral {
 	public static void main(String[] args) {
 		int gastosJaneiro = 15000;
 		int gastosFevereiro = 23000;
-		int gastosMarço = 17000;
+		int gastosMarÃ§o = 17000;
 		
-		int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarço; 
+		int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarÃ§o; 
 		
 		int media = gastosTrimestre / 3;
 		
-		System.out.println("O gasto trimestral é: " + gastosTrimestre);
-		System.out.println("Média: " + media);
+		System.out.println("O gasto trimestral Ã©: " + gastosTrimestre);
+		System.out.println("MÃ©dia: " + media);
 		System.out.println("Realizado o commit e push 11/03/19");
-
+		
+		System.out.println("auterado do git 1");
 	}
 
 }
